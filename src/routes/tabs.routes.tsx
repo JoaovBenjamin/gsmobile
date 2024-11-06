62718fdf691e1490ffb2489b9d0f8c6cf42e0cc0
Joaovvb <rm98938@fiap.com.br>
2024-11-06 16:13:43 -0300

    Tabs Navigator

--- src/routes/tabs.routes.tsx
+++ src/routes/tabs.routes.tsx
@@ -1,6 +1,7 @@
 import LoginScreen from '@/screen/LoginScreen';
 import RegisterScreen from '@/screen/RegisterScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { User, UserPlus } from 'lucide-react-native';
 
 
 const Tab = createBottomTabNavigator();
@@ -8,8 +9,24 @@ const Tab = createBottomTabNavigator();
 export default function TabsRoutes() {
   return (
     <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Login" component={LoginScreen} />
-      <Tab.Screen name="Register" component={RegisterScreen} />
+      <Tab.Screen 
+            name="Login" 
+            component={LoginScreen} 
+            options={{
+                tabBarIcon: ({color, size}) => (
+                    <User color={color} size={size}/>
+                )
+            }}
+            />
+      <Tab.Screen 
+            name="Register" 
+            component={RegisterScreen} 
+            options={{
+                tabBarIcon: ({color, size}) => (
+                    <UserPlus color={color} size={size}/>
+                )
+            }}
+            />
     </Tab.Navigator>
   );
 }
